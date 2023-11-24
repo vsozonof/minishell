4003563be872e0b4705575c12d36e93a09c6ff51
bravedinosaur <vr.sozonoff@gmail.com>
2023-11-24 18:19:56 +0100

    comments added

--- parsing/input_parser.c
+++ parsing/input_parser.c
@@ -6,7 +6,7 @@
 /*   By: vsozonof <vsozonof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 09:14:23 by vsozonof          #+#    #+#             */
-/*   Updated: 2023/11/23 17:11:47 by vsozonof         ###   ########.fr       */
+/*   Updated: 2023/11/24 18:19:33 by vsozonof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ void	input_parser(t_prompt *prompt)
 
 	i = 0;
 	init_str(&data, prompt);
-	while (prompt->input[i] && !ft_is_whitespace(prompt->input[i]))
-		i++;
-	c = 0;
 	while (prompt->input[i] && ft_is_whitespace(prompt->input[i]))
-	{
 		i++;
+	c = i;
+	while (prompt->input[c] && !ft_is_whitespace(prompt->input[c]))
 		c++;
-	}
-	data.input = ft_substr(data.input, i, c);
+	printf("i -> %i | c -> %i\n", i, c);
+	data.input = ft_substr(prompt->input, i, c);
+	i = c;
+	// ? La commande se retrouve dans data.input, que tu peux passer a une fonction
+	// ? pour exec
+	printf("cmd -> %s\n", data.input);
 }
