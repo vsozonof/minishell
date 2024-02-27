7bcdb5fb9c6ff177695681214792e4d81da5b06f
Victor SOZONOFF <vsozonof@bess-f3r4s11.clusters.42paris.fr>
2024-02-27 18:21:53 +0100

    fd tab free added

--- free/free_master.c
+++ free/free_master.c
@@ -6,7 +6,7 @@
 /*   By: vsozonof <vsozonof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 04:29:11 by vsozonof          #+#    #+#             */
-/*   Updated: 2024/02/26 17:44:28 by vsozonof         ###   ########.fr       */
+/*   Updated: 2024/02/27 18:21:04 by vsozonof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	free_master(t_data *data)
 		free(data->multi_inp);
 	}
 	free_cmd_nodes(data->exec);
+	free(data->here_doc_fd);
 }
 
 void	free_cmd_nodes(t_cmd *nodes)
